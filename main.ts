add654f9abf022be56b90edd9ab8a818fe4246fa
Bárbara Leimig <barbaraleimigdev@gmail.com>
2023-09-26 18:36:39 +0000

    tipos de variaveis

--- main.ts
+++ main.ts
@@ -2,7 +2,10 @@ let nome: string = "Barbara";
 let numero: number = 10;
 const pi: number = 3.14;
 
-let texto: string; /* Se você não for usar agora a variável, pode deixá-la tipada para que não 
+let texto:
+  | string
+  | number; /* Se você não for usar agora a variável, pode deixá-la tipada para que não 
 seja usado outro tipo de dado na mesma */
+texto = 10;
 
 console.log(`O nome é ${nome}`);
---
+++ tipos_variaveis.ts
@@ -0,0 +1,11 @@
+let n: number = 28;
+let variavel: null;
+let cor: string = "amarela";
+let info: boolean = true;
+let arr: string[] = ["maçã", "banana", "uva"];
+type Blusa = {
+  cor: string;
+  tamanho: string;
+};
+
+console.log(cor);
