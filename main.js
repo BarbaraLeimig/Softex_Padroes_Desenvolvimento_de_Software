4926eee33fa6aef7e066126f88930a1e688f2a4e
Bárbara Leimig <barbaraleimigdev@gmail.com>
2023-09-26 18:16:06 +0000

    tipos de dados

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 var nome = "Barbara";
+var numero = 10;
+var pi = 3.14;
+var texto; /* Se você não for usar agora a variável, pode deixá-la tipada para que não
+seja usado outro tipo de dado na mesma */
 console.log("O nome \u00E9 ".concat(nome));
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 let nome: string = "Barbara";
+let numero: number = 10;
+const pi: number = 3.14;
+
+let texto: string; /* Se você não for usar agora a variável, pode deixá-la tipada para que não 
+seja usado outro tipo de dado na mesma */
 
 console.log(`O nome é ${nome}`);
