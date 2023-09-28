bfe7b9056d5650a506ddd98eaa79cabcdc67ab81
Bárbara Leimig <barbaraleimigdev@gmail.com>
2023-09-28 17:59:19 +0000

    att

---
+++ metodos.ts
@@ -0,0 +1,6 @@
+let num: number = 28;
+
+console.log(n.toFixed(2));
+console.log(n.toPrecision(3));
+console.log(n.toString());
+console.log(n.toExponential(2));
--- tipos_variaveis.ts
+++ tipos_variaveis.ts
@@ -3,6 +3,9 @@ let variavel: null;
 let cor: string = "amarela";
 let info: boolean = true;
 let arr: string[] = ["maçã", "banana", "uva"];
+let lista: Array<any> = [1, 2, 3, 4];
+let list: Array<string> = new Array("1", "2", "3", "4");
+
 type Blusa = {
   cor: string;
   tamanho: string;
