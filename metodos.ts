c65e9d30a9aaca2f13ae94ba1d496255734016a2
Bárbara Leimig <barbaraleimigdev@gmail.com>
2023-09-28 19:17:35 +0000

    atts

--- metodos.ts
+++ metodos.ts
@@ -1,4 +1,4 @@
-let num: number = 28;
+/* let num: number = 28;
 console.log(num.toFixed(2));
 console.log(num.toPrecision(3));
 console.log(num.toString());
@@ -14,3 +14,25 @@ novastr = novastr.concat(music, txt);
 console.log(novastr);
 console.log(novastr.indexOf("s"));
 console.log(novastr.lastIndexOf("a"));
+ */
+
+let frutas: Array<string> = ["maçã", "banana", "uva", "morango"];
+
+frutas.push("laranja");
+console.log(frutas);
+
+let removido: string | undefined = frutas.pop();
+console.log(removido);
+
+frutas.unshift("laranjona");
+console.log(frutas);
+
+let frutaNova: Array<string> = ["melancia", "abacate", "melão"];
+let frutaria: Array<string> = [];
+frutaria = frutaria.concat(frutas, frutaNova);
+console.log(frutaria);
+
+frutaria.splice(1, 2, "jambo");
+console.log(frutaria);
+
+console.log(frutaria.indexOf("laranjona"));
