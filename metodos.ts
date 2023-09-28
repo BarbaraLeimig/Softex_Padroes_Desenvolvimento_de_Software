ad2ffeaa5ca411fe845308f2c6f56491ac93ee88
Bárbara Leimig <barbaraleimigdev@gmail.com>
2023-09-28 18:29:50 +0000

    novos metodos

--- metodos.ts
+++ metodos.ts
@@ -1,6 +1,16 @@
 let num: number = 28;
+console.log(num.toFixed(2));
+console.log(num.toPrecision(3));
+console.log(num.toString());
+console.log(num.toExponential(2));
 
-console.log(n.toFixed(2));
-console.log(n.toPrecision(3));
-console.log(n.toString());
-console.log(n.toExponential(2));
+let txt: string = "palavra guiada. ";
+console.log(txt.trim());
+console.log(txt.charAt(4));
+
+let music: string = "musica é uma ";
+let novastr: string = "";
+novastr = novastr.concat(music, txt);
+console.log(novastr);
+console.log(novastr.indexOf("s"));
+console.log(novastr.lastIndexOf("a"));
--- tipos_variaveis.ts
+++ tipos_variaveis.ts
@@ -3,7 +3,7 @@ let variavel: null;
 let cor: string = "amarela";
 let info: boolean = true;
 let arr: string[] = ["maçã", "banana", "uva"];
-let lista: Array<any> = [1, 2, 3, 4];
+let lista: Array<any> = [1, 2, 3, 4]; // any aceita qualquer tipo de dado
 let list: Array<string> = new Array("1", "2", "3", "4");
 
 type Blusa = {
